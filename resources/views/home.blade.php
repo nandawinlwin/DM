328054c99a46964b666df92ad8aa670cd3bb4709
nandawinlwin <nandawinlwin.mm@gmail.com>
2019-12-14 14:33:37 +0630

    Upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+ <!-- sidebar-wrapper  -->
+ <main class="page-content">
+    <div class="container-fluid">
+     
+
+    </div>
+  </main>
+  <!-- page-content" -->
+@endsection
